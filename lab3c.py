c61f154f71e6198b112f017657915003fcbe9a15
Edison Leung <eleung41@myseneca.ca>
2025-01-31 00:10:58 -0500

    added lab3c.py

---
+++ lab3c.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+'''Lab 3 Inv 2 function operate'''
+# Author ID: 147724231
+
+def operate(number1, number2, operator):
+    # Performs addition, subtraction, or multiplication on two numbers
+    if operator == 'add':
+        result = number1 + number2
+    elif operator == 'subtract':
+        result = number1 - number2
+    elif operator == 'multiply':
+        result = number1 * number2
+    else:
+        result = 'Error: function operator can be "add", "subtract", or "multiply"'
+    return result
+
+if __name__ == '__main__':
+    print(operate(10, 5, 'add'))       # Expected output: 15
+    print(operate(10, 5, 'subtract'))  # Expected output: 5
+    print(operate(10, 5, 'multiply'))  # Expected output: 50
+    print(operate(10, 5, 'divide'))    # Expected output: Error message
+
